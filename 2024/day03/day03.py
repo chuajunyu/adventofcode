a4c00948ac34f711f6001b8d65c61f30e461649f
chuajunyu <chuajunyu1@gmail.com>
2024-12-04 12:48:20 +0800

    Update helper docstring

--- 2024/day03/day03.py
+++ 2024/day03/day03.py
@@ -1,16 +1,9 @@
 def extract_input(filename):
     """
-    Reads a text file into a list of strings, where each string corresponds
-    to a line in the text file.
-
-    Removes the trailing newline at the end of each string.
+    Reads a text file and returns the content as a string.
     """
     with open(filename) as file:
         input = file.read()
-
-    # To remove the newline    
-    # for i in range(len(input)):
-    #     input[i] = input[i].strip('\n')
 
     return input
 
