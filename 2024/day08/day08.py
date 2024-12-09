2a55e92456dd4a0fb8ffa415a703e4470ad4f5cd
chuajunyu <chuajunyu1@gmail.com>
2024-12-09 21:36:40 +0800

    Add 2024 day 8

--- 2024/day08/day08.py
+++ 2024/day08/day08.py
@@ -56,15 +56,47 @@ def part1(input):
             
     return len(unique_antinode_set)
 
+
+def find_antinodes2(antenna_list, input):
+    antinode_set = set()
+    for antenna1 in antenna_list:
+        for antenna2 in antenna_list:
+            if antenna1 == antenna2:
+                continue
+            x1, y1 = antenna1
+            x2, y2 = antenna2
+            deltax = x2 - x1
+            deltay = y2 - y1
+            antinode1 = antenna1
+            antinode2 = antenna2
+
+            while antinode1[0] >= 0 and antinode1[1] >= 0 and antinode1[0] < len(input) and antinode1[1] < len(input[0]):
+                antinode_set.add(antinode1)
+                antinode1 = (antinode1[0] - deltax, antinode1[1] - deltay)
+                
+            while antinode2[0] >= 0 and antinode2[1] >= 0 and antinode2[0] < len(input) and antinode2[1] < len(input[0]):
+                antinode_set.add(antinode2)
+                antinode2 = (antinode2[0] + deltax, antinode2[1] + deltay)
+
+    return antinode_set
+
+
 def part2(input):
-    pass
+    antenna_map = get_antenna_map(input)
+    unique_antinode_set = set()
+    for antenna in antenna_map:
+        antinode_set = find_antinodes2(antenna_map[antenna], input)
+        unique_antinode_set = unique_antinode_set.union(antinode_set)
+            
+    return len(unique_antinode_set)
+
 
 if __name__ == "__main__":
     input = extract_input("input.txt")
     test_input = extract_input("test.txt")
 
-    print(part1(test_input))
-    print(part2(test_input))
+    # print(part1(test_input))  # Should be 14
+    # print(part2(test_input))  # Should be 34
 
-    print(part1(input))
-    print(part2(input))
+    print(f"Part 1: {part1(input)}")
+    print(f"Part 2: {part2(input)}")
